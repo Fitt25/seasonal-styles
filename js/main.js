f644351da138e6aa45e748dcd1b9ea8aed21351c
Linda <lindafittr@gmail.com>
2025-04-16 16:29:11 -0700

    Add style according to each season

--- js/main.js
+++ js/main.js
@@ -14,3 +14,44 @@ function myFunction() {
         x.className = "topnav";
     }
 }  
+function seasonStyle(season){
+    switch(season){
+        case "Spring":
+            $("body").css("background-color","#2B7129")
+            $("img#logo").attr("src", "images/spring.gif")
+            $("img#wear").attr("src", "images/spring-wear.jpg")
+            $("h3").text("Leap into our Spring Wear!")
+            break;
+        case "Summer":
+            $("body").css("background-color","#EBA52B")
+            $("img#logo").attr("src", "images/summer.gif")
+            $("img#wear").attr("src", "images/summer-wear.jpg")
+            $("h3").text("Leap into our Summer Wear!")
+            break;
+        case "Fall":
+            $("body").css("background-color","#A81124")
+            $("img#logo").attr("src", "images/fall.gif")
+            $("img#wear").attr("src", "images/fall-wear.jpg")
+            $("h3").text("Leap into our Fall Wear!")
+            break;
+        case "Winter":
+            $("body").css("background-color","#005393")
+            $("img#logo").attr("src", "images/winter.gif")
+            $("img#wear").attr("src", "images/winter-wear.jpg")
+            $("h3").text("Leap into our Winter Wear!")
+            break;
+        case "Default":
+            $("body").css("background-color","white")
+            $("img#logo").attr("src", "images/four-seasons.gif")
+            $("img#wear").attr("src", "images/300x400.png")
+            $("h3").text("Outfitter for All Seasons!")
+            break;
+    }
+}
+$("document").ready(function(){
+    $('li.seasons a').click(function(e){//find all a tags inside class of seasons
+        e.preventDefault();//stop default submission
+        var season = $(this).attr("href");//contents of href attribute of this element
+        seasonStyle(season)
+    });
+});
